2d6a5307609b7f5760439a0b1fb548e578b278b3
Lollipop50 <krololosh@gmail.com>
2020-04-25 16:40:16 +0300

    FeedAdapter and FeedViewHolder filling

--- app/src/main/java/com/lollipop50/croaker/feed/FeedAdapter.java
+++ app/src/main/java/com/lollipop50/croaker/feed/FeedAdapter.java
@@ -1,25 +1,59 @@
 package com.lollipop50.croaker.feed;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lollipop50.croaker.R;
+import com.lollipop50.croaker.model.Post;
+
+import java.util.List;
+
 public class FeedAdapter extends RecyclerView.Adapter<FeedViewHolder> {
+
+    private List<Post> allPosts;
+
+    private final ItemEventsListener itemEventsListener;
+
+    public FeedAdapter(List<Post> allPosts, ItemEventsListener itemEventsListener) {
+        this.allPosts = allPosts;
+        this.itemEventsListener = itemEventsListener;
+        setHasStableIds(true);
+    }
+
+    public void updateAllPosts(List<Post> updatedPosts) {
+        allPosts = updatedPosts;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return allPosts.get(position).getId().hashCode();
+    }
 
     @NonNull
     @Override
     public FeedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_post, parent, false);
+        return new FeedViewHolder(itemView, itemEventsListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FeedViewHolder holder, int position) {
-
+        holder.bidnTo(allPosts.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return allPosts.size();
+    }
+
+    interface ItemEventsListener {
+        void onItemClick(Post post);
+        void onLongItemClick(Post post);
     }
 }
--- app/src/main/java/com/lollipop50/croaker/feed/FeedViewHolder.java
+++ app/src/main/java/com/lollipop50/croaker/feed/FeedViewHolder.java
@@ -1,13 +1,62 @@
 package com.lollipop50.croaker.feed;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lollipop50.croaker.R;
+import com.lollipop50.croaker.model.Post;
+
 public class FeedViewHolder extends RecyclerView.ViewHolder {
 
-    public FeedViewHolder(@NonNull View itemView) {
+    private ImageView avatarView;
+    private TextView usernameView;
+    private TextView postTextView;
+    private ImageView postPictureView;
+    private CheckBox isLikedView;
+
+    private final FeedAdapter.ItemEventsListener itemEventsListener;
+
+    private Post currentPost;
+
+    public FeedViewHolder(@NonNull View itemView, final FeedAdapter.ItemEventsListener itemEventsListener) {
         super(itemView);
+
+        this.itemEventsListener = itemEventsListener;
+
+        avatarView = itemView.findViewById(R.id.avatar_view);
+        usernameView = itemView.findViewById(R.id.username_view);
+        postTextView = itemView.findViewById(R.id.post_text_view);
+        postPictureView = itemView.findViewById(R.id.post_picture_view);
+        isLikedView = itemView.findViewById(R.id.is_liked_view);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemEventsListener.onItemClick(currentPost);
+            }
+        });
+
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                itemEventsListener.onLongItemClick(currentPost);
+                return true;
+            }
+        });
+    }
+
+    public void bidnTo(Post post) {
+        currentPost = post;
+
+        // Add avatar
+        usernameView.setText(currentPost.getUsername());
+        postTextView.setText(currentPost.getPostText());
+        // Add postPicture
+        isLikedView.setChecked(currentPost.isLiked());
     }
 }
