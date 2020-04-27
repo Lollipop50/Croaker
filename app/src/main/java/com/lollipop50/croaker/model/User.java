3e24512f3b9422f669aefacb6bb714059d34a139
Lollipop50 <krololosh@gmail.com>
2020-04-27 17:21:10 +0300

    Adding User class and layout for PostDetailsFragment

---
+++ app/src/main/java/com/lollipop50/croaker/model/User.java
@@ -0,0 +1,47 @@
+package com.lollipop50.croaker.model;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+    private String avatar;
+    private String username;
+    private String bio;
+
+    public User() {
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
---
+++ app/src/main/java/com/lollipop50/croaker/post_details/PostDetailsFragment.java
@@ -0,0 +1,6 @@
+package com.lollipop50.croaker.post_details;
+
+import androidx.fragment.app.Fragment;
+
+public class PostDetailsFragment extends Fragment {
+}
